edb25385c3fad33f7d2bd501b1142b94a980486c
Alexis Claudé <alex.claude.33@gmail.com>
2026-06-22 12:53:55 -0400

    mis à jour de main.py pour s'adapter à la structure de things_cfr

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import warnings
 import logging
 from pathlib import Path
 import torch
+import gc
 
 from Config import Config
 from HDF5Writer import HDF5Writer
@@ -22,23 +23,24 @@ if __name__ == '__main__':
     config.charger_env()
 
     ROOT = Path(__file__).parent.parent
-    DATA_DIR = ROOT / "things-cfr"
+    DATA_DIR = ROOT / "data" / "things_mp4_cfr"
     HDF5_DIR = ROOT / "output/hdf5/things_encoding"
 
     model = config.charger_modele()
     fmri_enc = model.__pydantic_private__['_model']
 
     writer = HDF5Writer(HDF5_DIR)
-    video_files = sorted(DATA_DIR.glob("sub-*_*_task-*.mp4"))
+    video_files = sorted(DATA_DIR.rglob("sub-*_*_task-*_desc-CFR.mp4"))
 
     for video_path in video_files:
         parts = video_path.stem.split("_")
-        subject = parts[0]
-        session = parts[1]
-        run = video_path.stem
+        subject = parts[0]  # "sub-01"
+        session = parts[1]  # "ses-001"
+
+        run = next((p for p in parts if p.startswith("run-")), "run-unknown")
 
         if not subject.startswith("sub-") or not session.startswith("ses-"):
-            print(f"✗ Structure invalide : {video_path.name}")
+            print(f"✗ Structure invalide : {video_path.name}",flush=True)
             continue
 
         try:
@@ -55,7 +57,16 @@ if __name__ == '__main__':
 
             writer.sauvegarder(features, preds, subject, session, run)
 
-            print(f"✓ {subject}/{session}/{run} - preds shape: {preds.shape}")
+            print(f"✓ {subject}/{session}/{run} - preds shape: {preds.shape}",flush=True)
+
+            # Nettoyage de la mémoire pour la vidéo suivante
+            del features, preds, segments, events, hooks
+
+            gc.collect()
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+
         except Exception as e:
-            print(f"✗ {subject}/{session}/{run} - Erreur: {e}")
+            print(f"✗ {subject}/{session}/{run} - Erreur: {e}",flush=True)
+
 
